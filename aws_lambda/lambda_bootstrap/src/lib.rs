fbd1154c8eba5eab73a34037d86e186824a52ccc
Manu <gnork99@hotmail.com>
2022-07-08 10:47:08 -0700

    Delete aws_lambda directory

--- aws_lambda/lambda_bootstrap/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod lambda;
-pub mod events;
-
-use events::aws_lambda;
-use lambda::event_loop;
-
-pub fn start(lambda: fn(&aws_lambda::LambdaEvent) -> String) {
-    event_loop::start_loop(lambda);
-}
